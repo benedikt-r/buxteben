e717951eeca0b67a5f4d97a3f8626c09a0f5c36c
benedikt-r <benedikt.rudolf@gmx.net>
2017-11-16 14:04:32 +0100

    5.1 Changing Routes New Product-order

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ end
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to "/orders", notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :products
+
+  get 'products_path/products'
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
 
-  get 'simple_pages/index'
-
   root 'simple_pages#landing_page'
-
+  
   resources :orders, only: [:index, :show, :create, :destroy]
 
 end
