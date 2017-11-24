106fc53e9845e4103fb3ee9d94a790db6dd26a55
benedikt-r <benedikt.rudolf@gmx.net>
2017-11-24 14:41:21 +0100

    5.10 admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,21 @@
+
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    user ||= User.new #guest user (not logged in)
+    if user.admin?
+        can :manage, :all
+    else
+        can %i[show update], User, id: user.id
+        can %i[show index], Order, user_id: user.id
+        can %i[destroy create], OrderProduct, order_id: user.active_order.id
+        can :create, Comment, user_id: user.id
+        can :show, Comment
+    end
   end
-
+  
 end
-
-
-
-
 
 
 
