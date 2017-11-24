fa6f95a1d63ffe5c057aeecb73b5ef5e2939ae37
benedikt-r <benedikt.rudolf@gmx.net>
2017-11-24 10:11:57 +0100

    5.8 Adding comments and raty.js

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 
+  def average_rating
+  comments.average(:rating).to_f
+  end
+
   def highest_rating_comment
    comments.rating_desc.first
   end
