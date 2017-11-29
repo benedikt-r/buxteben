f132ddf9115fef630cc4dca9c80706495f514df5
benedikt-r <benedikt.rudolf@gmx.net>
2017-11-29 13:52:36 +0100

    5.11 improve code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,5 +28,5 @@ end
 private
 
   def comment_params
-    params.require(:comment).permit(:user_id, :body, :rating)
+    params.require(:comment).permit( :body, :rating)
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,6 @@ class Ability
     else
         can %i[show update], User, id: user.id
         can %i[show index], Order, user_id: user.id
-        can %i[destroy create], OrderProduct, order_id: user.active_order.id
         can :create, Comment, user_id: user.id
         can :show, Comment
     end
