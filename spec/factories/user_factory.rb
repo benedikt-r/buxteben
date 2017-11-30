8a6469b34548514ea18ff53bab7c75ca2b761526
benedikt-r <benedikt.rudolf@gmx.net>
2017-11-30 16:10:33 +0100

    6.4 adding factories final

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,17 +2,14 @@ FactoryBot.define do
  sequence(:email) { |n| "user#{n}@example.com" }
 
 factory :user do
-    email 
-    password
-    first_name 
-    last_name
-    admin false
-  end
+        email 
+        password "123456"
+    end
+
+    
 factory :admin, class: User do
-  email 
-  password
-  admin true
-  first_name 
-  last_name
-  end
+      email 
+      password "123456"
+      admin true
+    end
 end
