4c17b10a446ff1677dc00fc24ea990d95123d890
benedikt-r <benedikt.rudolf@gmx.net>
2017-11-30 15:14:14 +0100

    6.4 add Factories

--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
+  @user = FactoryBot.create(:user)
+
+ # let(:user) { User.create!(email: 'sss@ss.com', password: '1234') }
 
   describe 'GET #show' do
      context 'when a user is logged in' do
      	before do
-        sign_in user
+        sign_in @user
       end
       it 'loads correct user details' do
-        get :show, params: { id: user.id }
-        expect(assigns(:user)).to eq user
+        get :show, params: { id: @user.id }
+        expect(assigns(:user)).to eq @user
         expect(response).to have_http_status(200)
       end
 
@@ -18,7 +20,7 @@ describe UsersController, type: :controller do
 
      context 'when a user is not logged in' do
        it 'redirects to login' do
-         get :show, params: { id: user.id }
+         get :show, params: { id: @user.id }
          expect(response).to redirect_to("http://test.host/")
        end
      end
