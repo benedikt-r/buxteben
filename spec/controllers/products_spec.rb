be03b26587f2a784fa2f4912032181634d3b5170
benedikt-r <benedikt.rudolf@gmx.net>
2017-12-08 10:55:30 +0100

    6.15.Updating specs

--- spec/controllers/products_spec.rb
+++ spec/controllers/products_spec.rb
@@ -9,4 +9,53 @@ describe ProductsController, type: :controller do
       expect(response).to render_template('index')
     end
   end
+
+
+    context 'POST #create' do
+    it 'is an invalid product' do
+    @product = FactoryBot.build(:product, name: "")
+    expect(@product).not_to be_valid
+    end
+  end
+
+  context 'GET #show' do
+    it 'renders the show page' do
+      product = FactoryBot.create(:product)
+      get :show, id: product.id
+      expect(response).to be_ok
+      expect(response).to render_template('show')
+    end
+  end
+
+  context "DELETE #destroy" do
+    before do
+      @user = FactoryBot.build(:admin)
+      sign_in @user
+    end
+
+    it "should allow admin to delete product" do
+      product = FactoryBot.create(:product)
+      delete :destroy, id: product.id
+      expect(response).to redirect_to products_path
+    end
+  end
+
+  context "put #update" do
+    before do
+      @product = FactoryBot.create(:product)
+      @user = FactoryBot.build(:user)
+      sign_in @user
+    end
+    it "successfully updates a product" do
+      @update = { name:@product.name, image_url:"images.com", id:@product.id, price:@product.price, colour: "brown"}
+      put :update, params: { id: @product.id, product: @update}
+      @product.reload
+      expect(@product.colour).to eq "brown"
+    end
+  end
+
 end
+
+
+
+
