d5cdff15e075c894d08fb25c6d7c63cebe895e2f
benedikt-r <benedikt.rudolf@gmx.net>
2017-12-08 20:52:45 +0100

    6.15.updating specs

--- spec/controllers/products_spec.rb
+++ spec/controllers/products_spec.rb
@@ -1,46 +1,45 @@
 require 'rails_helper'
-
-
+ 
+ 
 describe ProductsController, type: :controller do
   context 'GET #index' do
-    it 'renders the index page' do 
+    it 'renders the index page' do
       get :index
       expect(response).to be_ok
       expect(response).to render_template('index')
     end
   end
-
-
+ 
+ 
     context 'POST #create' do
     it 'is an invalid product' do
     @product = FactoryBot.build(:product, name: "")
-    post :create, params: { id: @product.id, product: @create}
     expect(@product).not_to be_valid
     end
   end
-
+ 
   context 'GET #show' do
     it 'renders the show page' do
       product = FactoryBot.create(:product)
-      get :show, params:{} product.id}
+      get :show, params: {id: product.id}
       expect(response).to be_ok
       expect(response).to render_template('show')
     end
   end
-
+ 
   context "DELETE #destroy" do
     before do
       @user = FactoryBot.build(:admin)
       sign_in @user
     end
-
+ 
     it "should allow admin to delete product" do
       product = FactoryBot.create(:product)
-      expect {delete :destroy, id: product.id }.to change (Product, :count).by(-1)
+      expect { delete :destroy, params: {id: product.id} }.to change(Product, :count).by(-1)
       expect(response).to redirect_to products_path
     end
   end
-
+ 
   context "put #update" do
     before do
       @product = FactoryBot.create(:product)
@@ -51,12 +50,11 @@ describe ProductsController, type: :controller do
       @update = { name:@product.name, image_url:"images.com", id:@product.id, price:@product.price, color: "brown"}
       put :update, params: { id: @product.id, product: @update}
       @product.reload
-      expect(@product.colour).to eq "brown"
+      expect(@product.color).to eq "brown"
     end
   end
-
+ 
 end
-
 
 
 
---
+++ spec/factories/product_factory.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :product do
+        name "name"
+        description "description"
+        image_url "image.jpg"
+        price 123
+        color "black"
+    end
+ 
+end
